5b2b41772dd72d7759f6644fc05c7116118ee606
kumar7ashutosh <kumarashutoshbtech2023@gmail.com>
2025-12-30 22:28:23 +0530

    zenml

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,18 +6,23 @@ from src.exception import MyException
 from src.config import CONFIG
 
 @step
-def load_data()->pd.DataFrame:
-    config=CONFIG['data']
-    dataset_url=config['source_URL']
-    df=pd.read_csv(dataset_url)
+def load_data() -> pd.DataFrame:
+    path = CONFIG["data"]["local_data_file"]
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    
+    if not os.path.exists(path):
+        url = CONFIG["data"]["source_URL"]
+        df = pd.read_csv(url)
+        df.to_csv(path, index=False)
+    else:
+        df = pd.read_csv(path)
+    
     return df
 
 @step
-def save_data(df:pd.DataFrame)->str:
-    config=CONFIG['data']
-    local_csv_path=config['local_data_file']
-    os.makedirs(os.path.dirname(local_csv_path),exist_ok=True)
-    df.to_csv(local_csv_path,index=False)
-    return local_csv_path
+def save_data(df: pd.DataFrame) -> str:
+    path = CONFIG["data"]["local_data_file"]
+    df.to_csv(path, index=False)
+    return path
 
 
